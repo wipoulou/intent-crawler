54b0d79719ee1dd0880da9cb19fdfde1dc81091e
wipoulou <bazinloic@hotmail.fr>
2021-10-05 15:06:43 +0200

    Fix module import

--- app/providers/carrefour.py
+++ app/providers/carrefour.py
@@ -1,4 +1,4 @@
-from provider import Provider
+from providers.provider import Provider
 
 
 class Carrefour(Provider):
--- app/providers/franprix.py
+++ app/providers/franprix.py
@@ -1,4 +1,4 @@
-from provider import Provider
+from providers.provider import Provider
 
 """
 Franprix has an API, so it doesn't fit the scrapping requirement, but still interesting to get the data.
